bf600762484f40974a57f86461f4926f005ea559
Matt Oswalt <matt@keepingitclassless.net>
2014-02-20 14:12:16 -0800

    Fixed a stupid syntax error. What can I say - I'm just a networking guy.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Copyright (C) 2013 Matt Oswalt (http://keepingitclassless.net/) 
 #Obviously humorous and meant to be taken as such.
 
-answer = input("Can you use a GUI? (yes/no)")
+answer = raw_input("Can you use a GUI? (yes/no)")
 
-if answer == 'no'
+if answer == 'no':
 	print "That's awkward."
-elif answer == 'yes'
+elif answer == 'yes':
 	print "Congratulations! You're a networking expert!"
