a2cf24d6c37bf7143e5e2766a0470322ca56bb99
Nick Buraglio <nick@buraglio.com>
2014-02-20 15:18:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #Copyright (C) 2013 Matt Oswalt (http://keepingitclassless.net/) 
 #Obviously humorous and meant to be taken as such.
 
-answer = raw_input("Can you use a GUI? (yes/no/huh)")
+import webbrowser
+
+answer = raw_input("Can you use a GUI? (yes/no/huh/hello)")
 
 if answer == 'no':
-	print "That's awkward."
+	webbrowser.open_new('http://www.opendaylight.org/')
 elif answer == 'yes':
 	print "Congratulations! You're a networking expert!"
-elif answer == "hello?":
+elif answer == "hello":
 	print "I see that you are in management. Go fire some CCIEs or something."
 elif answer == "huh":
 	print "What's a goooey ?"
